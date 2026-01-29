a63d95ad19ac98264ce080bdc15204d2d5e23c8f
Gabriel da Silva Marques <gdasilv2@student13.cse.nd.edu>
2026-01-28 19:33:02 -0500

    add: auto docker setup

---
+++ g_install/clone_git.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+alr_installed=""
+
+if [ -n "$(find . -maxdepth 1 -type d -name "gerbil-software" -print -quit 2>/dev/null)" ]; then
+    alr_installed="1"
+    echo "True, all set."
+    echo "Now you will need to install docker for your OS. Go to https://www.docker.com/get-started/ and get Docker. Once you finish setting it up, press 1. "
+    read reader_input
+    clean_input=$(echo "$reader_input" | tr -cd '0-9')
+
+    if [ "$reader_input" = "1" ]; then	    
+	echo "Connecting to Docker (this might take a while for the first time)"
+        docker compose up
+	exit 0
+    else
+        echo "Input different than 1. Exiting."
+        exit 1
+    fi
+
+    exit 0
+else
+    alr_installed="0"
+    echo "False, cloning repo."
+    git clone https://github.com/Domer-Rover/gerbil-software
+    exit 1
+fi
