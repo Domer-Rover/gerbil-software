63e1e8cdbc7fd6c780b3392d333d77ce0c27ebe5
Gabriel da Silva Marques <gdasilv2@student13.cse.nd.edu>
2026-02-15 16:44:57 -0500

    update clone_git.sh

--- g_install/clone_git.sh
+++ g_install/clone_git.sh
@@ -4,13 +4,15 @@ alr_installed=""
 
 if [ -n "$(find . -maxdepth 1 -type d -name "gerbil-software" -print -quit 2>/dev/null)" ]; then
     alr_installed="1"
-    echo "True, all set."
-    echo "Now you will need to install docker for your OS. Go to https://www.docker.com/get-started/ and get Docker. Once you finish setting it up, press 1. "
+    echo "You are all set with git cloning!"
+    echo ""
+    echo "Now you will need to install docker for your OS. Go to https://www.docker.com/get-started/ and get Docker. Once you finish setting it up, press 1 here in the terminal. "
     read reader_input
     clean_input=$(echo "$reader_input" | tr -cd '0-9')
 
     if [ "$reader_input" = "1" ]; then	    
 	echo "Connecting to Docker (this might take a while for the first time)"
+	echo ""
         docker compose up
 	exit 0
     else
@@ -21,7 +23,8 @@ if [ -n "$(find . -maxdepth 1 -type d -name "gerbil-software" -print -quit 2>/de
     exit 0
 else
     alr_installed="0"
-    echo "False, cloning repo."
+    echo "Repo not cloned in you machine. Cloning repo..."
+    echo ""
     git clone https://github.com/Domer-Rover/gerbil-software
     exit 1
 fi
